cd8b2ba79bd81f6f543acb99c5ce456c7d09ea9f
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-03-19 15:35:13 -0400

    base api

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+const baseURL = "https://pokeapi.co/api/v2/";
 
-fetch(baseUrl)
+let tab1 = document.querySelector(".tab1");
+
+
+let showPokemon = (e) => {
+    let inputValue = e.target.elements[0].value
+    let fullURL = `${baseURL}pokemon/${inputValue}`;
+
+fetch(fullURL)
     .then(res => res.json())
     .then(res => {
-        console.log("success", res)
+        let grabMainBox = document.getElementsByClassName(".mainbox");
+        let pokeImg = document.querySelector(".image");
+        pokeImg.setAttribute("src", res[0].url)
+        grabMainBox.appendChild(pokeImg);
     })
+    
     .catch(err => console.log("Oops! Pokeball unavailable. Try again!", err));
 
 
+}
+
+    // fetch(fullURL)
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         let grabBody = document.querySelector('body')
+    //         let createHeader = document.createElement('h1')
+
+    //         createHeader.innerHTML = res.name
+    //         grabBody.appendChild(createHeader)
+    //     })
+
+tab1.addEventListener("click", showPokemon)
